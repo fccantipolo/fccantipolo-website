768597e29b890502329884d1117c4bc5d82b9e92
Melvin Morato Borja <melvinmoratoborja@gmail.com>
2020-02-25 15:34:32 +0800

    fix(navbar): Open menu on mobile

--- src/layouts/Navbar.vue
+++ src/layouts/Navbar.vue
@@ -39,3 +39,23 @@
     </div>
   </nav>
 </template>
+
+<script>
+export default {
+  mounted () {
+    // Get the "navbar-burger" element
+    const el = document.querySelector('.navbar-burger')
+
+    el.addEventListener('click', () => {
+
+      // Get the target from the "data-target" attribute
+      const target = el.dataset.target
+      const $target = document.getElementById(target)
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      el.classList.toggle('is-active')
+      $target.classList.toggle('is-active')
+    })
+  }
+}
+</script>
