a9ed1c98313dac4516e0a574605dee1fdea7549f
Melvin Morato Borja <melvinmoratoborja@gmail.com>
2020-02-25 15:01:17 +0800

    Create pages

---
+++ src/pages/Resources.vue
@@ -0,0 +1,14 @@
+<template>
+  <Layout>
+    <section class="section is-medium">
+      <div class="container has-text-centered">
+        <h1 class="title ">
+          Coming soon!
+        </h1>
+        <p class="description">
+          We are currently working on it.
+        </p>
+      </div>
+    </section>
+  </Layout>
+</template>
