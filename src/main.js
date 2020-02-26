caa0639ca67be0d90d17aca68a2dbbe8560fd856
Melvin Morato Borja <melvinmoratoborj@gmail.com>
2020-02-27 01:00:41 +0800

    Add Open Graph tags

--- src/main.js
+++ src/main.js
@@ -10,6 +10,34 @@ export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 
+  // Add OGP tags
+  head.meta.push(
+    {
+      property: 'og:site_name',
+      content: 'freeCodeCamp Antipolo'
+    },
+    {
+      property: 'og:title',
+      content: 'freeCodeCamp Antipolo'
+    },
+    {
+      property: 'og:description',
+      content: 'A local study group in Antipolo City learning to code through freeCodeCamp.org'
+    },
+    {
+      property: 'og:image',
+      content: 'https://fccantipolo.netlify.com/banner.png'
+    },
+    {
+      property: 'og:url',
+      content: 'https://fccantipolo.netlify.com'
+    },
+    {
+      property: 'og:type',
+      content: 'website'
+    }
+  )
+
   // Add has-navbar-fixed-top class to body
   head.bodyAttrs = { class: 'has-navbar-fixed-top' }
 }
